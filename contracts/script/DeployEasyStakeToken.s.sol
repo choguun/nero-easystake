fdf6a7d688efdd0986f029db1ad4ca9d4ef17801
choguun <visaruth.s@gmail.com>
2025-06-02 23:49:10 -0700

    add easystaketoken

---
+++ contracts/script/DeployEasyStakeToken.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/EasyStakeToken.sol";
+
+contract DeployEasyStakeToken is Script {
+    function run() external returns (EasyStakeToken) {
+        vm.startBroadcast();
+        // The initial owner will be the address executing this script
+        EasyStakeToken easyStakeToken = new EasyStakeToken(msg.sender);
+        vm.stopBroadcast();
+        console.log("EasyStakeToken deployed at:", address(easyStakeToken));
+        return easyStakeToken;
+    }
+} 
